aa1ca7dd3964c5ec8c4e64c2fe459aac06000b72
Corentin Derbré <poubelle.co@free.fr>
2019-01-31 22:38:12 +0800

    diff parser

--- stun.js
+++ stun.js
@@ -8,7 +8,14 @@ const bodyParser = require('body-parser');
  
 // parse application/json
 // app.use(bodyParser.json())
-app.use(bodyParser.raw({type: '*/*', limit: '15mb'}))
+// app.use(bodyParser.raw({type: '*/*', limit: '15mb'}))
+
+app.use (function(req, res, next) {
+  req.rawBody = '';
+  req.setEncoding('utf8');
+  req.on('data', function(chunk) { req.rawBody += chunk });
+  next()
+})
 
 app.use(cors())
 
